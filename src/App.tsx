8c2d7df6140e1f6cc38648feae5cb5f1f0e90833
Ella-Kim913 <hyunkk0913@gmail.com>
2024-03-14 11:19:31 -0700

    1.1 Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,55 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./compoments/layout"
+import Home from "./routes/home"
+import Profile from "./routes/propfile";
+import Login from "./routes/login";
+import Create_Account from "./routes/create-account";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />
+      }
+
+    ]
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/create-account",
+    element: <Create_Account />
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+  body {
+    background-color: black;
+    color:white;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+`;
+
 function App() {
-  return <></>;
+  return <>
+    <GlobalStyles />
+    <RouterProvider router={router} />
+  </>;
 }
 
 export default App
